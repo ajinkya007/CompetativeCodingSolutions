c8430cd0fde115ccad4065e24f189224ffa27910
ajinkya007 <ajinkya007.in@gmail.com>
2016-09-09 21:38:54 +0530

    SPOJ: PRLADDU.c-1

---
+++ SPOJ/PRLADDU.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main() {
+	int T;
+	long long int D[10000], units;
+	int i,j,x,n;
+	scanf("%d", &T);
+	for(x=0;x<T;x++) {
+		scanf("%d", &n);
+		for(i=0;i<n;i++) {
+			scanf("%lld", &D[i]);
+		}
+		i = 0;
+		units = 0;
+		s}
+	printf("%lld\n", units);
+	}
+}
